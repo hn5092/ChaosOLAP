ce14e7b974453f65fc376879896729854ee51f33
yiming.xu <100650920@qq.com>
2022-07-25 08:50:42 +0800

    add c lib

---
+++ cpp/src/library.cpp
@@ -0,0 +1,8 @@
+#include "library.h"
+
+#include <iostream>
+#include "jni.h"
+
+void hello() {
+	std::cout << "Hello, World!" << std::endl;
+}
---
+++ java/untitled/src/main/java/test/TestMain.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.load("/Users/xuyiming/git/ChaosOLAP/cpp/cmake-build-debug/src/libcpp.dylib");
+    }
+}
